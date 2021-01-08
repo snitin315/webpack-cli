2c3aa44f386f7abec2c6d10425bb504edddce6e2
Nitin Kumar <snitin315@gmail.com>
2021-01-08 14:40:55 +0530

    refactor(serve): fix typesript types and warnings

--- packages/configtest/src/index.ts
+++ packages/configtest/src/index.ts
@@ -27,7 +27,7 @@ class ConfigTestCommand {
                         throw new webpack.WebpackOptionsValidationError(error);
                     }
                 } catch (error) {
-                    const isValidationError = (error) => {
+                    const isValidationError = (error): boolean => {
                         // https://github.com/webpack/webpack/blob/master/lib/index.js#L267
                         // https://github.com/webpack/webpack/blob/v4.44.2/lib/webpack.js#L90
                         // eslint-disable-next-line @typescript-eslint/no-explicit-any
